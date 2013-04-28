39e2053e633ddf1261f29e58b31dd1aba85f3df1
W. Owen Parry <paradigmsort@gmail.com>
2013-04-28 15:21:18 -0400

    Username in title, h1 of user page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,15 @@ describe "UserPages" do
     it { should have_selector('h1', text: "Sign up") }
     it { should have_selector('title', text: full_title("Sign up")) }
   end
+
+  describe "User page" do
+    before {
+      @user = User.new(name:"Example User", email: "example@user.com", password: "abcabc", password_confirmation: "abcabc")
+      @user.save
+      visit user_path(@user)
+    }
+
+    it { should have_selector('h1', text: @user.name) }
+    it { should have_selector('title', text: @user.name) }
+  end
 end
