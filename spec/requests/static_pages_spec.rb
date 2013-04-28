40c1ebdad46c814ec831981352a6a06efd84a36f
W. Owen Parry <paradigmsort@gmail.com>
2013-04-28 10:40:22 -0400

    Helper for page titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,13 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-
   subject { page }
 
   describe "Home page" do
     before { visit root_path }
 
     it { should have_selector('h1', :text => 'Sample App') }
-    it { should have_selector('title', :text => "#{base_title}") }
+    it { should have_selector('title', :text => full_title("")) }
     it { should_not have_selector('title', :text => "|") }
   end
 
@@ -18,21 +16,21 @@ describe "StaticPages" do
     before { visit help_path }
 
     it { should have_selector('h1', :text => 'Help') }
-    it { should have_selector('title', :text => "#{base_title} | Help") }
+    it { should have_selector('title', :text => full_title("Help")) }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_selector('h1', :text => 'About Us') }
-    it { should have_selector('title', :text => "#{base_title} | About") }
+    it { should have_selector('title', :text => full_title("About")) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_selector('h1', :text => 'Contact Us') }
-    it { should have_selector('title', :text => "#{base_title} | Contact") }
+    it { should have_selector('title', :text => full_title("Contact")) }
   end
 
 end
