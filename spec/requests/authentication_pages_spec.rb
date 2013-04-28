1611e885f87bde31aa5515c58ffabe7bdf6a9bd4
W. Owen Parry <paradigmsort@gmail.com>
2013-04-28 18:05:05 -0400

    Correct errors in test

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,8 +7,8 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_selector('title'), text: "Sign in" }
-    it { should have_selector('h1'), text: "Sign in" }
+    it { should have_selector('title', text: "Sign in") }
+    it { should have_selector('h1', text: "Sign in") }
 
     describe "with invalid info" do
       describe "after sumission" do
