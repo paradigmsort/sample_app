8a69a7330ac33871fa30f3f44986e06681c287a2
W. Owen Parry <paradigmsort@gmail.com>
2013-04-28 11:58:15 -0400

    Tests for header + footer links

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,3 +6,17 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+def test_title(title)
+  page.should have_selector 'title', text: full_title(title)
+  if title.empty?
+    page.should_not have_selector 'title', text: "|"
+  end
+end
+
+def test_link(link_name,target_title)
+  visit path # return to the page under test
+  click_link link_name
+  # check we were linked to a page with the given title
+  test_title(target_title)
+end
