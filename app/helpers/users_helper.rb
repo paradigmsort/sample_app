d556a0beeddbd4d01127832901f47a7e3ee27a71
W. Owen Parry <paradigmsort@gmail.com>
2013-04-28 15:39:29 -0400

    Add gravatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for(user)
+    digest = Digest::MD5::hexdigest(user.email.downcase)
+    uri = "https://secure.gravatar.com/avatar/#{digest}"
+    image_tag(uri, alt: user.name, class: "gravatar" )
+  end
 end
