bc65155b64f2484b4bb16e1ee80dd728d2d4a815
W. Owen Parry <paradigmsort@gmail.com>
2013-04-28 23:21:31 -0400

    User factory outputs sequence

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name "Owen Parry"
-    email "paradigmsort@gmail.com"
+    sequence(:name) { |n| "Person #{n}"}
+    sequence(:email) { |n| "person-#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
   end 
