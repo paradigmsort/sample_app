34481c0c41eab8169eb48d4b73c499135a0331ff
W. Owen Parry <paradigmsort@gmail.com>
2013-04-28 21:45:47 -0400

    Edit user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,9 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
+      flash[:success] = "Profile successfully changed."
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end
