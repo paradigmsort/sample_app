f2334e651e7aa4ec2c41f83a64c230fc48ceeb7c
W. Owen Parry <paradigmsort@gmail.com>
2013-04-28 22:21:46 -0400

    Enforce correct user for update/edit actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -98,6 +98,22 @@ describe "UserPages" do
     end
   end
 
+  describe "as wrong user" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:wrong_user) { FactoryGirl.create(:user, email:"wrong@user.com") }
+    before { sign_in user }
+
+    describe "visiting the edit page" do
+      before { visit edit_user_path(wrong_user) }
+      it { should_not have_selector("title", text: "Edit user") }
+    end
+
+    describe "submitting to the update action" do
+      before { put user_path(wrong_user) }
+      specify { response.should redirect_to(root_path) }
+    end
+  end
+
   describe "User page" do
     let(:user) { FactoryGirl.create(:user) }
     before {  visit user_path(user) }
