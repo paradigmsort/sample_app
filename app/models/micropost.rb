7b869f65edb63dd7bd0cc9a5117452174b993512
W. Owen Parry <paradigmsort@gmail.com>
2013-05-01 08:27:18 -0400

    Microposts

---
+++ app/models/micropost.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Micropost < ActiveRecord::Base
+  attr_accessible :content
+
+  validates :user_id, presence: true
+  validates :content, presence: true
+end
---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,39 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'spec_helper'
+
+describe Micropost do
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+   @micropost = Micropost.new(content:"Lorem ipsum")
+   @micropost.user_id = user.id
+ end
+
+  subject { @micropost }
+
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
+
+  it { should be_valid }
+
+  describe "when user id is missing" do
+    before { @micropost.user_id = nil}
+
+    it { should_not be_valid }
+  end
+
+  describe "when content is empty" do
+    before { @micropost.content = " " }
+
+    it { should_not be_valid }
+  end
+end
