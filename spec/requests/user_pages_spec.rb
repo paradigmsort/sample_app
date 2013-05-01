66420b1ee5275e154e65c7b53e5aad6dea82b935
W. Owen Parry <paradigmsort@gmail.com>
2013-04-30 20:27:37 -0400

    Factor fields for create/update user forms

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,7 +29,7 @@ describe "UserPages" do
         fill_in "Name", with: "Example User"
         fill_in "Email", with: "example@user.com"
         fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
@@ -70,7 +70,7 @@ describe "UserPages" do
         fill_in "Name", with: new_name
         fill_in "Email", with: new_email
         fill_in "Password", with: user.password
-        fill_in "Confirmation", with: user.password
+        fill_in "Confirm Password", with: user.password
         click_button "Save changes"
       end
 
