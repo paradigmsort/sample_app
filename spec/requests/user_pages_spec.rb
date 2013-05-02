ad02edcffe800c3c1a39dc61e29ba41598694e97
W. Owen Parry <paradigmsort@gmail.com>
2013-05-02 08:19:49 -0400

    Microposts on user page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -186,9 +186,17 @@ describe "UserPages" do
 
   describe "User page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Post 1") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Post 2") }
     before {  visit user_path(user) }
 
     it { should have_main_heading(user.name) }
     it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 end
