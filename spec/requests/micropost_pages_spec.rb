1016cdd4568e70d4bdf6f667df9cc406c4111f89
W. Owen Parry <paradigmsort@gmail.com>
2013-05-02 23:17:09 -0400

    Show feed on home page

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -72,8 +72,19 @@ describe "MicropostPages" do
           end
         end
       end
-
     end
 
+    describe "feed" do
+      before do
+        2.times { FactoryGirl.create(:micropost, user: user) }
+        visit root_path
+      end
+
+      it "should show the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
 end
