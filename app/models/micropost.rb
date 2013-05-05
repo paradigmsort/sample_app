bd68dc5ae22f7e946a959cd9920ca7a2d1248b79
W. Owen Parry <paradigmsort@gmail.com>
2013-05-05 15:07:47 -0400

    Feed via subselect

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -19,6 +19,8 @@ class Micropost < ActiveRecord::Base
   default_scope order: 'microposts.created_at DESC'
 
   def self.from_users_followed_by(user)
-    where("user_id in (?) or user_id = ?", user.followed_users, user.id)
+    followed_user_ids = "SELECT followed_id FROM follow_relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user.id)
   end
 end
