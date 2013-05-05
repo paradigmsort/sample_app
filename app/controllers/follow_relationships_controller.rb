7fecb5487cbe55b5f87772d499eeaad6c0f8a384
W. Owen Parry <paradigmsort@gmail.com>
2013-05-05 14:29:52 -0400

    Follow/unfollow with ajax

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -3,12 +3,18 @@ class FollowRelationshipsController < ApplicationController
   def create
     @user = User.find(params[:follow_relationship][:followed_id])
     current_user.follow!(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
     @user = FollowRelationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
